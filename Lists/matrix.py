09589bc76ed0ceeae146b8f1438b4b57392028b3
AndreiP <lolatiautalent@gmail.com>
2023-08-30 09:48:36 +0100

    matrix

---
+++ Lists/matrix.py
@@ -0,0 +1,13 @@
+''' 
+Matrix A 
+
+[[1, 2, 3] 
+[4, 5, 6] 
+[7, 8, 9]] 
+
+Find the sum of all the columns and rows and create separate lists 
+
+output: sum of columns: [ 12, 15, 18] 
+sum of rows: [6, 15, 24] 
+'''
+
